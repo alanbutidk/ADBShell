642c05421959a4a3743a7bfa81010de8fbc8abf8
Alan <sybaualan67@gmail.com>
2026-06-14 14:44:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Links (temp):
+//Dynam:https://github.com/alanbutidk/ADBShell/releases/download/v1.0.0/adbshell.exe
+//stat:https://github.com/alanbutidk/ADBShell/releases/download/v1.0.0/staticadbshell.exe
+//zipbundle:https://github.com/alanbutidk/ADBShell/releases/download/v1.0.0/ADBShell_Windows_x86_64.zip
+//Repo:https://github.com/alanbutidk/ADBShell
+//.zipsrc:https://github.com/alanbutidk/ADBShell/archive/refs/tags/v1.0.0.zip
+//.gzsrc:https://github.com/alanbutidk/ADBShell/archive/refs/tags/v1.0.0.tar.gz
+
